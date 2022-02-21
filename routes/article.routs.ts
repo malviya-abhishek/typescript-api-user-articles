21d487e36f29a4dfb136e88b8bf1ca51a91a48a9
Abhishek Malviya <71405686+malviya-abhishek@users.noreply.github.com>
2022-02-22 01:13:49 +0530

    article routes

---
+++ routes/article.routs.ts
@@ -0,0 +1,10 @@
+import {Express} from 'express'
+import { articleController } from '../controllers/article.controllers'
+
+export const userRoutes = function (app:Express) {
+    app.get("/articles", [articleController.getArticles]);
+    app.get("/articles/:articleId", [articleController.getArticle]);
+    app.post("/articles", [articleController.createArticle]);
+    app.put("/articles/:articleId", [articleController.updateArticle]);
+    app.delete("/articles/:articleId", [articleController.deleteArticle]);
+}
