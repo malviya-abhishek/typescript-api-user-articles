2292933a81acbc31bd42d10d165133255b63e7bc
Abhishek Malviya <71405686+malviya-abhishek@users.noreply.github.com>
2022-02-22 01:13:23 +0530

    articles routes

---
+++ controllers/article.controllers.ts
@@ -0,0 +1,31 @@
+import {Request, Response} from 'express';
+import db from '../models'
+
+const getArticle = (req: Request, res: Response) => {
+    
+}
+
+const getArticles = (req: Request, res: Response) => {
+    
+}
+
+const createArticle = (req: Request, res: Response) => {
+    
+}
+
+const updateArticle = (req: Request, res: Response) => {
+    
+}
+
+const deleteArticle = (req: Request, res: Response) => {
+    
+}
+
+
+export const articleController = {
+    getArticle,
+    getArticles,
+    createArticle,
+    updateArticle,
+    deleteArticle
+};
